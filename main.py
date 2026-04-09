32d498a16cca0a2280a5aa81b129b68bf3a41bc3
JSelemin <j.selemin@gmail.com>
2026-04-09 19:26:37 -0300

    create volatility, market dominance, and top movers functions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.extract import fetch_coin_charts
-from src.transform import create_staging_table, create_daily_returns, create_rolling_averages
+from src.transform import create_staging_table, create_daily_returns, create_rolling_averages, create_volatility, create_market_dominance, create_top_movers
 
 coins_to_fetch = ["bitcoin", "ethereum", "solana", "tron", "dogecoin"]
 
@@ -13,8 +13,19 @@ def main():
         #create_daily_returns(coins_to_fetch)
         print('Daily returns done.')
 
-        create_rolling_averages(coins_to_fetch)
+        #create_rolling_averages(coins_to_fetch)
         print('Rolling averages done.')
+
+        #create_volatility(coins_to_fetch)
+        print('Volatility done.')
+
+        #create_market_dominance(coins_to_fetch)
+        print('Market dominance done.')
+
+        create_top_movers(coins_to_fetch)
+        print('Top movers done.')
+
+
 
 if __name__ == "__main__":
         main()
--- src/transform.py
+++ src/transform.py
@@ -66,3 +66,78 @@ def create_rolling_averages(COINS):
     
     rolling_averages_table.write_parquet('data/analyses/rolling_averages.parquet')    
     rolling_averages_table.show()
+
+def create_volatility(COINS):
+
+    staging_table = duckdb.read_parquet('data/analyses/daily_returns.parquet')
+
+    select_cols = []
+    final_table = duckdb.sql("SELECT date FROM staging_table")
+
+    for token in COINS:
+        token_staging_table = duckdb.sql(f"SELECT date, {token}_price, {token}_daily_returns, ROUND(STDDEV({token}_daily_returns) OVER (ORDER BY date ROWS BETWEEN 29 PRECEDING AND CURRENT ROW), 2) as {token}_monthly_volatility FROM staging_table")
+        final_table = duckdb.sql("SELECT * FROM final_table LEFT JOIN token_staging_table ON final_table.date = token_staging_table.date")
+        select_cols.extend([f"{token}_price", f"{token}_daily_returns", f"{token}_monthly_volatility"])
+    
+    select_clause = "date, " + ", ".join(select_cols)
+    volatility_table = duckdb.sql(f"SELECT {select_clause} FROM final_table")
+    
+    volatility_table.write_parquet('data/analyses/volatility.parquet')    
+    volatility_table.show()
+
+def create_market_dominance(COINS):
+
+    staging_table = duckdb.read_parquet('data/staging_table.parquet')
+
+    columns_array = []
+    for token in COINS:
+        columns_array.extend([f"{token}_market_cap"])
+
+    columns_clause = "date, " + ", ".join(columns_array)
+    sum_clause = " + ".join(columns_array)
+    middle_table = duckdb.sql(f'SELECT {columns_clause}, {sum_clause} AS total_market_cap FROM staging_table')
+    middle_table.show()
+
+    select_cols = []
+    final_table = duckdb.sql("SELECT date FROM staging_table")
+
+    for token in COINS:
+        token_staging_table = duckdb.sql(f"SELECT date, total_market_cap, {token}_market_cap, ROUND(({token}_market_cap / total_market_cap) * 100, 2) AS {token}_dominance FROM middle_table")
+        final_table = duckdb.sql("SELECT * FROM final_table LEFT JOIN token_staging_table ON final_table.date = token_staging_table.date")
+        select_cols.extend([f"{token}_market_cap", f"{token}_dominance"])
+    
+    select_clause = "date, " + "total_market_cap, " + ", ".join(select_cols)
+    market_dominance_table = duckdb.sql(f"SELECT {select_clause} FROM final_table")
+    
+    market_dominance_table.write_parquet('data/analyses/market_dominance.parquet')    
+    market_dominance_table.show()
+
+def create_top_movers(COINS):
+
+    staging_table = duckdb.read_parquet('data/analyses/daily_returns.parquet')
+
+    columns_array = []
+    absolutes_array = []
+
+    for token in COINS:
+        columns_array.extend([f"{token}_daily_returns"])
+        absolutes_array.extend([f"ABS({token}_daily_returns)"])
+
+    absolutes = ", ".join(absolutes_array)
+    case_array = []
+    naming_case_array = []
+
+    for token in COINS:
+        case_array.extend([f"WHEN ABS({token}_daily_returns) = GREATEST({absolutes}) THEN {token}_daily_returns"])
+        naming_case_array.extend([f"WHEN ABS({token}_daily_returns) = GREATEST({absolutes}) THEN '{token}'"])
+
+
+    columns  = ", ".join(columns_array)
+    cases = " ".join(case_array)
+    naming_cases = " ".join(naming_case_array)
+
+    helper_table = duckdb.sql(f"SELECT *, YEARWEEK(date) AS yearwk, CASE {cases} END AS top_mover, CASE {naming_cases} END AS coin_name FROM staging_table")
+    helper_second_table = duckdb.sql(f"SELECT MAX(ABS(top_mover)) AS week_mover, yearwk FROM helper_table GROUP BY yearwk")
+    final_table = duckdb.sql(f"SELECT f.yearwk AS year_week, s.week_mover AS max_daily_movement, f.coin_name AS coin, f.date AS date_of_move FROM helper_second_table s LEFT JOIN helper_table f ON s.week_mover = ABS(top_mover) AND s.yearwk = f.yearwk")
+    final_table.show()
+    final_table.write_parquet('data/analyses/top_movers.parquet')
